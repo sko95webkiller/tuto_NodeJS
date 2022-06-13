4265dfe6ff0dd5b3a58cca66d1468b92decde99a
sko <sko@toto.fr>
2022-06-13 09:29:12 +0200

    Creating a node js simple server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const port = 3000
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.write("<h1>Welcome to server.js</h1>") 
+    res.end()
+})
+server.listen(port,()=>{console.log(`node.js simple server is launched at :http://localhost:${port}...`)})
+
