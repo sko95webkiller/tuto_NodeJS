28047ddb0b4fa9eb3dd4fa6a53da4b887784cfae
sko95webkiller <77539037+sko95webkiller@users.noreply.github.com>
2022-06-13 10:37:37 +0200

    Change the port nimber to 4000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const port = 3500
+const port = 4000
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/html"})
     res.write("<h1>Welcome to server.js</h1>") 
