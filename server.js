a4efcf98b313a73ea6b4339be448b4d49b1239cf
sko <sko@toto.fr>
2022-06-13 10:21:06 +0200

    Modify port number to 3500

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const port = 3000
+const port = 3500
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/html"})
     res.write("<h1>Welcome to server.js</h1>") 
